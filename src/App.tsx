1a606c7e761531f2d8f3b290380886427e97527e
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-04-23 22:51:46 +0530

    feat: fixed meta tags, other styling elements
    Added shadcn/ui support, custom fonts, changed tailwind config file and other minor adjustments for styling.
    
    Also fixed meta tags for SEO

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 const App = () => {
-	return <div>App</div>
+	return (
+		<div className='w-screen min-h-screen text-white bg-zinc-900 p-4'>
+			<h1 className='text-2xl md:text-3xl font-semibold text-center font-mono'>
+				Discord Timestamp Generator
+			</h1>
+		</div>
+	)
 }
 
 export default App
