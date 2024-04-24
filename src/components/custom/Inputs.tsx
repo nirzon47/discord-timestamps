469b4a4da31d3d0b719d3dfd9fb41aefd95eebde
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-04-24 23:34:03 +0530

    feat: added select option
    Added Select field and changed some components styles. Added labels for SEO and accessibility.

--- src/components/custom/Inputs.tsx
+++ src/components/custom/Inputs.tsx
@@ -1,5 +1,13 @@
+import { Label } from '@radix-ui/react-label'
 import { DatePicker } from '../ui/date-picker'
 import { Input } from '../ui/input'
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from '@/components/ui/select'
 
 interface Props {
 	date: Date | undefined
@@ -8,18 +16,59 @@ interface Props {
 }
 
 const Inputs = (props: Props) => {
+	const handleTimeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+		props.setTime(e.target.value)
+	}
+
 	return (
 		<section className='mt-16'>
-			<h2 className='text-xl font-light mb-4 text-center md:text-left'>
-				Enter Date and Time
-			</h2>
-			<div className='flex flex-col gap-2'>
-				<DatePicker date={props.date} setDate={props.setDate} />
-				<Input
-					type='time'
-					className='w-[240px] justify-start text-left font-normal rounded-lg hover:bg-accent bg-popover'
-					onChange={(e) => props.setTime(e.target.value)}
-				/>
+			<div className='flex flex-col gap-4'>
+				<div className='flex flex-col gap-px'>
+					<Label htmlFor='date' className='text-sm opacity-65'>
+						Enter Date
+					</Label>
+					<DatePicker
+						id='date'
+						date={props.date}
+						setDate={props.setDate}
+						className='-mt-4'
+					/>
+				</div>
+				<div className='flex flex-col gap-px'>
+					<Label htmlFor='time' className='text-sm opacity-65'>
+						Enter Time
+					</Label>
+					<Input
+						id='time'
+						type='time'
+						className='w-[240px] justify-start rounded-lg bg-popover text-left font-normal hover:bg-accent'
+						onChange={handleTimeInput}
+					/>
+				</div>
+				<div>
+					<Label htmlFor='options' className='text-sm opacity-65'>
+						Select Format
+					</Label>
+					<Select>
+						<SelectTrigger
+							id='options'
+							className='w-[240px] rounded-lg bg-popover text-left font-normal duration-200 hover:bg-accent'
+						>
+							<SelectValue placeholder='Select Format' />
+						</SelectTrigger>
+						<SelectContent>
+							<SelectItem value='relative'>Relative</SelectItem>
+							<SelectItem value='shortTime'>Short Time</SelectItem>
+							<SelectItem value='longTime'>Long Time</SelectItem>
+							<SelectItem value='shortDate'>Short Date</SelectItem>
+							<SelectItem value='longDate'>Long Date</SelectItem>
+							<SelectItem value='longDateShortTime'>
+								Long Date with Short Time
+							</SelectItem>
+							<SelectItem value='full'>Full Format</SelectItem>
+						</SelectContent>
+					</Select>
+				</div>
 			</div>
 		</section>
 	)
--- src/components/ui/date-picker.tsx
+++ src/components/ui/date-picker.tsx
@@ -21,7 +21,7 @@ export function DatePicker({ date, setDate }: any) {
 						!date && 'text-muted-foreground'
 					)}
 				>
-					<CalendarIcon className='mr-2 h-4 w-4' />
+					<CalendarIcon className='mr-2 size-4' />
 					{date ? format(date, 'PPP') : <span>Pick a date</span>}
 				</Button>
 			</PopoverTrigger>
