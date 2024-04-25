a7d11c338b40cfc9715892dc066d1c1a12f0ba79
Nirzon Taru Karmakar <nirzonk47@gmail.com>
2024-04-25 16:45:21 +0530

    feat: added format preview
    Fixed some accessibility issues and changed orientation for labels. Also added a preview format for the selected format.

--- src/components/custom/Inputs.tsx
+++ src/components/custom/Inputs.tsx
@@ -13,6 +13,7 @@ interface Props {
 	date: Date | undefined
 	setDate: React.Dispatch<React.SetStateAction<Date | undefined>>
 	setTime: React.Dispatch<React.SetStateAction<string | undefined>>
+	setFormat: React.Dispatch<React.SetStateAction<string | undefined>>
 }
 
 const Inputs = (props: Props) => {
@@ -20,11 +21,15 @@ const Inputs = (props: Props) => {
 		props.setTime(e.target.value)
 	}
 
+	const handleFormatSelect = (e: string) => {
+		props.setFormat(e)
+	}
+
 	return (
 		<section className='mt-16'>
 			<div className='flex flex-col gap-4'>
 				<div className='flex flex-col gap-px'>
-					<Label htmlFor='date' className='text-sm opacity-65'>
+					<Label htmlFor='date' className='text-center text-sm opacity-65'>
 						Enter Date
 					</Label>
 					<DatePicker
@@ -35,7 +40,7 @@ const Inputs = (props: Props) => {
 					/>
 				</div>
 				<div className='flex flex-col gap-px'>
-					<Label htmlFor='time' className='text-sm opacity-65'>
+					<Label htmlFor='time' className='text-center text-sm opacity-65'>
 						Enter Time
 					</Label>
 					<Input
@@ -46,12 +51,17 @@ const Inputs = (props: Props) => {
 					/>
 				</div>
 				<div>
-					<Label htmlFor='options' className='text-sm opacity-65'>
+					<Label
+						id='optionsLabel'
+						htmlFor='options'
+						className='inline-block w-full text-center text-sm opacity-65'
+					>
 						Select Format
 					</Label>
-					<Select>
+					<Select onValueChange={handleFormatSelect}>
 						<SelectTrigger
 							id='options'
+							aria-labelledby='optionsLabel'
 							className='w-[240px] rounded-lg bg-popover text-left font-normal duration-200 hover:bg-accent'
 						>
 							<SelectValue placeholder='Select Format' />
--- src/components/custom/Main.tsx
+++ src/components/custom/Main.tsx
@@ -1,15 +1,36 @@
 import { useState } from 'react'
 import Inputs from './Inputs'
 
+const formatExamples: Record<string, string> = {
+	relative: '2 days ago',
+	shortTime: '2:30 PM',
+	longTime: '2:30:45 PM',
+	shortDate: '02/02/2023',
+	longDate: 'February 2, 2023',
+	longDateShortTime: '2 February 2023 2:30 PM',
+	full: 'Friday, February 2, 2023, 2:30:45 PM',
+}
+
 const Main = () => {
 	const [date, setDate] = useState<Date>()
 	const [time, setTime] = useState<string>()
+	const [format, setFormat] = useState<string>()
 
-	console.log(time)
+	console.log(time, format)
 
 	return (
 		<main className='grid justify-center'>
-			<Inputs date={date} setDate={setDate} setTime={setTime} />
+			<Inputs
+				date={date}
+				setDate={setDate}
+				setTime={setTime}
+				setFormat={setFormat}
+			/>
+			<div className='mt-4 h-8 text-center'>
+				{format && (
+					<p className='text-lg opacity-80'>{formatExamples[format]}</p>
+				)}
+			</div>
 		</main>
 	)
 }
